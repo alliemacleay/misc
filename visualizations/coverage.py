d3429c07e85ca7234c8102e128888b7c3ee88e0d
alliemacleay <allison.macleay+csedu@gmail.com>
2018-09-24 11:40:15 -0400

    Update coverage

--- visualizations/coverage.py
+++ visualizations/coverage.py
@@ -1,3 +1,7 @@
+"""
+(c) MGH Center for Integrated Diagnostics
+"""
+
 from __future__ import print_function
 from __future__ import absolute_import
 import matplotlib.pyplot as plt
@@ -6,17 +10,20 @@ from matplotlib import colors
 import matplotlib.collections as collections
 import pandas as pd
 from pyensembl import EnsemblRelease
+from math import ceil
 
 __author__ = 'Allison MacLeay'
 
 
-class FusionViz(object):
-    def __init__(self, chrom, start, stop, hist):
+class CoverageViz(object):
+    """Creates a coverage bar chart and highlights exons """
+    def __init__(self, chrom, start, stop, hist, metaval=None):
         self.chrom = chrom
         self.start = start
         self.stop = stop
-        self.dict = self._to_dict(hist)  # hist is [x, y]
+        self.dict = self._to_dict(hist)  # hist is [x, y] where x is position and y is coverage
         self.hist = self.xy()
+        self.metaval = '{}\n'.format(metaval) if metaval else ''
 
     def _to_dict(self, hist):
         """ pad coverage data to match length of region """
@@ -34,11 +41,12 @@ class FusionViz(object):
         return [x, y]
 
     def plot(self, saveto=False):
+        """ Create plot """
         lhist = len(self.hist[0])
 
         plt.subplot(2, 1, 1)
         n, fracs, patches = plt.hist(self.invert(), bins=lhist/10)
-        plt.title('Coverage')
+        plt.title('ALK coverage')
         plt.ylabel('colored histogram')
 
         # We'll color code by height, but you could use any scalar
@@ -52,17 +60,26 @@ class FusionViz(object):
             color = plt.cm.viridis(norm(thisfrac))
             thispatch.set_facecolor(color)
         barplot_sp = plt.subplot(2, 1, 2)
-        _ = plt.bar(self.hist[0], self.hist[1])
-        exon_array = self.parse_ref_exons()
-        coll = collections.BrokenBarHCollection.span_where(self.hist[0], ymin=0, ymax=350, where=exon_array > 0, color='g')
-        barplot_sp.add_collection(coll)
-        plt.xticks(np.arange(self.start, self.stop, 1000))
-        plt.xlabel('position')
-        plt.ylabel('coverage')
-        plt.show()
+        self.exon_plot(plt, barplot_sp)
         if saveto:
             plt.savefig(saveto)
             print('Plot saved to {}'.format(saveto))
+        plt.show()
+
+    def exon_plot(self, plt, subplot):
+        _ = plt.bar(self.hist[0], self.hist[1])
+        exon_array, exon_df = self.parse_ref_exons()
+        ymax = max(self.hist[1])
+        coll = collections.BrokenBarHCollection.span_where(self.hist[0], ymin=0, ymax=ymax, where=exon_array > 0,
+                                                           color='#41e0c8')
+        subplot.add_collection(coll)
+        plt.xticks(np.arange(self.start, self.stop, 1000))
+        plt.xlabel('position')
+        plt.ylabel('{}coverage'.format(self.metaval))
+        factor = ceil(float(ymax) / 7)
+        for i, row in exon_df.iterrows():
+            offset = factor + (i % 2) * factor
+            plt.text(row['start'], ymax - offset, 'exon {}'.format(row['number']), color='#e04641')
 
     def invert(self):
         iv = []
@@ -70,9 +87,6 @@ class FusionViz(object):
             for x in range(self.hist[1][i]):
                 iv.append(a + self.start)
         return iv
-
-    def d3(self):
-        pass
 
     def load_ensembl_ref(self, ens_db, version, rid=None):
         """ Download, load, and index ensembl data """
@@ -84,15 +98,26 @@ class FusionViz(object):
             return None
 
     def get_exon_numbers(self, ens_db, gene):
+        """ This creates exon areas
+
+        , but the numbering is off """
         dct = {'start': [], 'id': []}
-        e = ens_db.exon_ids_of_gene_name(gene)
+        gene_id = ens_db.gene_ids_of_gene_name(gene)[0]
+        transcripts = ens_db.transcript_ids_of_gene_id(gene_id)
+        longest = 0
+        e = None
+        for t in transcripts:
+            tsc = ens_db.exon_ids_of_transcript_id(t)
+            sz = len(tsc)
+            if sz > longest:
+                longest = sz
+                e = tsc
         for exid in e:
             exon = ens_db.exon_by_id(exid)
             dct['start'].append(exon.start)
             dct['id'].append(exid)
         df = pd.DataFrame(dct)
-        df = df.sort('start')
-        df['number'] = range(1, df.shape[0] + 1)
+        df['number'] = df.index + 1
         return df
 
     def parse_ref_exons(self):
@@ -106,7 +131,6 @@ class FusionViz(object):
         exon_array = np.zeros(self.stop - self.start)
         exon_numbers = self.get_exon_numbers(ens_db, exons[0].gene_name)
         for exobj in exons:
-            ex_number = exon_numbers[exon_numbers['id'] == exobj.id]['number'].values[0]
             start = exobj.start - self.start
             stop = exobj.end - self.start
             i = start
@@ -114,8 +138,28 @@ class FusionViz(object):
                 exon_array[i] = 1
                 i += 1
         # 2:29,448,326-29,448,432 exon 19
-        return exon_array
+        # exon 22 start: 29445210
+        # exon 18 end: 29449940
+        # intron 19: 29446395-29448326
+        # ATI initiation 29446768-29448326
+        return exon_array, exon_numbers[(exon_numbers['start'] > self.hist[0][0]) &
+                                        (exon_numbers['start'] < self.hist[0][-1])]
 
 
+class VizList(object):
+    def __init__(self, vizlist):
+        self.vizlist = vizlist
+
+    def report(self, output_file):
+        nsub = len(self.vizlist)
+        fig = plt.figure(figsize=[24, 20])
+        for i, viz in enumerate(self.vizlist):
+            sub = fig.add_subplot(nsub, 1, i + 1)
+            viz.exon_plot(plt, sub)
+
+        fig.tight_layout()
+        fig.savefig(output_file)
+        fig.show()
+        pass
 
 
